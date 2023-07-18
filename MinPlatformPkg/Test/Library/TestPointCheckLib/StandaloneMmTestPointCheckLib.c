30c54d2decf255dcb9bd0739ad8c17d56267c243
Ken Pong <b-jpeng@microsoft.com>
2023-07-18 19:21:06 +0800

    [Test Automation] Change ImplementationID to corresponding ID for DXE/SMM

--- MinPlatformPkg/Test/Library/TestPointCheckLib/StandaloneMmTestPointCheckLib.c
+++ MinPlatformPkg/Test/Library/TestPointCheckLib/StandaloneMmTestPointCheckLib.c
@@ -99,7 +99,7 @@ TestPointReadyToBootMmPageProtection (
   if (Result) {
     TestPointLibSetFeaturesVerified (
       PLATFORM_TEST_POINT_ROLE_PLATFORM_IBV,
-      NULL,
+      TEST_POINT_IMPLEMENTATION_ID_PLATFORM_SMM,
       TEST_POINT_INDEX_BYTE6_SMM,
       TEST_POINT_BYTE6_SMM_READY_TO_BOOT_SMM_PAGE_LEVEL_PROTECTION
       );
@@ -116,7 +116,7 @@ TestPointReadyToBootMmPageProtection (
     if (Result) {
       TestPointLibSetFeaturesVerified (
         PLATFORM_TEST_POINT_ROLE_PLATFORM_IBV,
-        NULL,
+        TEST_POINT_IMPLEMENTATION_ID_PLATFORM_SMM,
         TEST_POINT_INDEX_BYTE6_SMM,
         TEST_POINT_BYTE6_SMM_READY_TO_LOCK_SECURE_SMM_COMMUNICATION_BUFFER
         );
@@ -159,7 +159,7 @@ TestPointReadyToLockMmMemoryAttributeTableFunctional (
   if (Result) {
     TestPointLibSetFeaturesVerified (
       PLATFORM_TEST_POINT_ROLE_PLATFORM_IBV,
-      NULL,
+      TEST_POINT_IMPLEMENTATION_ID_PLATFORM_SMM,
       TEST_POINT_INDEX_BYTE6_SMM,
       TEST_POINT_BYTE6_SMM_READY_TO_LOCK_SMM_MEMORY_ATTRIBUTE_TABLE_FUNCTIONAL
       );
@@ -275,14 +275,14 @@ TestPointReadyToBootMmPageProtectionHandler (
     if (Result) {
       TestPointLibSetFeaturesVerified (
         PLATFORM_TEST_POINT_ROLE_PLATFORM_IBV,
-        NULL,
+        TEST_POINT_IMPLEMENTATION_ID_PLATFORM_SMM,
         TEST_POINT_INDEX_BYTE6_SMM,
         TEST_POINT_BYTE6_SMM_READY_TO_LOCK_SECURE_SMM_COMMUNICATION_BUFFER
         );
     } else {
       TestPointLibClearFeaturesVerified (
         PLATFORM_TEST_POINT_ROLE_PLATFORM_IBV,
-        NULL,
+        TEST_POINT_IMPLEMENTATION_ID_PLATFORM_SMM,
         TEST_POINT_INDEX_BYTE6_SMM,
         TEST_POINT_BYTE6_SMM_READY_TO_LOCK_SECURE_SMM_COMMUNICATION_BUFFER
         );
